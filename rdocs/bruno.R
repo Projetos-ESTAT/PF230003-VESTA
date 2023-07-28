32b9bc08936ef091cc6711145676c22d0d7a841e
Bruno Gondim Toledo <toledis@gmail.com>
2023-07-28 20:24:44 -0300

    .

--- rdocs/bruno.R
+++ rdocs/bruno.R
@@ -22,3 +22,30 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+df <- data.frame(read_excel("banco/df.xlsx")) # leitura dos dados ----
+
+#df$GRUPO.MÁSCARA
+
+df <- df |>
+  filter(GRUPO.MÁSCARA == 1 | GRUPO.MÁSCARA == 2) # eliminando os dropouts do estudo
+
+df$Desfecho.positivo..Influenza..durante.o.acompanhamento. <- ifelse(df$Desfecho.positivo..Influenza..durante.o.acompanhamento.
+ == "SIM","Sim",df$Desfecho.positivo..Influenza..durante.o.acompanhamento.) # ajustando as respostas
+
+table(factor(df[,30])) # Teste positivo para influenza
+table(factor(df[,31])) # Teste positivo para covid
+
+df1 <- df |>
+  filter(GRUPO.MÁSCARA == 1)
+
+table(factor(df1[,30])) # Teste positivo para influenza
+table(factor(df1[,31])) # Teste positivo para covid
+
+df2 <- df |>
+  filter(GRUPO.MÁSCARA == 2)
+
+table(factor(df2[,30])) # Teste positivo para influenza
+table(factor(df2[,31])) # Teste positivo para covid
+
+# ---------------------------------------------------------------------------- #
