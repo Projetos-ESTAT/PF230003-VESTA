9b1e7a695a044dcfe5947a978f153cc084a56354
Bruno Gondim Toledo <toledis@gmail.com>
2023-08-17 19:43:01 -0300

    .

--- rdocs/coxas.R
+++ rdocs/coxas.R
@@ -82,7 +82,45 @@ df$`Altura (em cm)`=as.numeric(df$`Altura (em cm)`)
 df$IMC= as.numeric(df$IMC)
 
 p_load(compareGroups)
+
+# Transformando as variáveis para o formato correto:
 df$`GRUPO MÁSCARA` <- factor(df$`GRUPO MÁSCARA`)
+df$Sexo <- factor(df$Sexo)
+df$`Estado em que nasceu` <- factor(df$`Estado em que nasceu`)
+df$`Estado civil` <- factor(df$`Estado civil`)
+df$Escolaridade <- factor(df$Escolaridade)
+df$`Raça declarada` <- factor(df$`Raça declarada`)
+df$`Profissão:` <- factor(df$`Profissão:`)
+df$`Local de trabalho 1` <- factor(df$`Local de trabalho 1`)
+df$`Qual sua carga horária de trabalho semanal no 'Local de trabalho 1'?` <- factor(df$`Qual sua carga horária de trabalho semanal no 'Local de trabalho 1'?`)
+df$`Há quanto tempo trabalha no 'Local de trabalho 1'?` <- factor(df$`Há quanto tempo trabalha no 'Local de trabalho 1'?`)
+df$`Setor onde trabalha no 'Local de trabalho 1'` <- factor(df$`Setor onde trabalha no 'Local de trabalho 1'`)
+df$`Trabalha em mais de um local?` <- factor(df$`Trabalha em mais de um local?`)
+df$`Local de trabalho 2` <- factor(df$`Local de trabalho 2`)
+df$`Setor onde trabalha no Local de trabalho 2:` <- factor(df$`Setor onde trabalha no Local de trabalho 2:`)
+df$`Qual sua carga horária de trabalho semanal no local de trabalho 2?` <- as.numeric(df$`Qual sua carga horária de trabalho semanal no local de trabalho 2?`)
+df$`Há quanto tempo (em anos) trabalha no Local de trabalho 2?` <- as.numeric(df$`Há quanto tempo (em anos) trabalha no Local de trabalho 2?`)
+df$`É fumante?` <- factor(df$`É fumante?`)
+df$Situação <- factor(df$Situação)
+df$`Resultado RT-PCR COVID (A1)` <- factor(df$`Resultado RT-PCR COVID (A1)`)
+df$`Resultado RT-PCR COVID (A2)` <- factor(df$`Resultado RT-PCR COVID (A2)`)
+df$`Resultado RT-PCR Influenza A e painel viral (A1)` <- factor(df$`Resultado RT-PCR Influenza A e painel viral (A1)`)
+df$`Resultado RT-PCR Influenza A e painel viral (A2)` <- factor(df$`Resultado RT-PCR Influenza A e painel viral (A2)`)
+df$`Resultado RT-PCR Influenza B e painel viral (A1)` <- factor(df$`Resultado RT-PCR Influenza B e painel viral (A1)`)
+df$`Resultado RT-PCR Influenza B e painel viral (A2)` <- factor(df$`Resultado RT-PCR Influenza B e painel viral (A2)`)
+df$`Desfecho positivo (COVID-19) durante o acompanhamento?` <- factor(df$`Desfecho positivo (COVID-19) durante o acompanhamento?`)
+df$`Desfecho positivo (Influenza) durante o acompanhamento?` <- factor(df$`Desfecho positivo (Influenza) durante o acompanhamento?`)
+df$`Tem histórico da COVID-19 ou TR (IgG+) positivos?` <- factor(df$`Tem histórico da COVID-19 ou TR (IgG+) positivos?`)
+df$`Já foi diagnosticado com COVID-19 mais de uma vez?` <- factor(df$`Já foi diagnosticado com COVID-19 mais de uma vez?`)
+df$`STATUS VACINAL- A0: Já tomou vacina contra a COVID-19? (A0)` <- factor(df$`STATUS VACINAL- A0: Já tomou vacina contra a COVID-19? (A0)`)
+df$`Quantas doses da vacina contra a COVID-19 já tomou?  (A0)` <- as.numeric(df$`Quantas doses da vacina contra a COVID-19 já tomou?  (A0)`)
+df$`Tomou a vacina contra a gripe (este ano)? (A0)` <- factor(df$`Tomou a vacina contra a gripe (este ano)? (A0)`)
+df$`STATUS VACINAL- A1: Já tomou vacina contra a COVID-19?` <- factor(df$`STATUS VACINAL- A1: Já tomou vacina contra a COVID-19?`)
+df$`Quantas doses da vacina contra a COVID-19 já tomou? (A1)` <- as.numeric(df$`Quantas doses da vacina contra a COVID-19 já tomou? (A1)`)
+df$`Tomou a vacina contra a gripe (este ano)? (A1)` <- factor(df$`Tomou a vacina contra a gripe (este ano)? (A1)`)
+df$`STATUS VACINAL - A2: Já tomou vacina contra a COVID-19?` <- factor(df$`STATUS VACINAL - A2: Já tomou vacina contra a COVID-19?`)
+df$`Quantas doses da vacina contra a COVID-19 já tomou? (A2)` <- as.numeric(df$`Quantas doses da vacina contra a COVID-19 já tomou? (A2)`)
+
 #Fazendo os teste de comparação entre os grupos
 result=compareGroups(`GRUPO MÁSCARA` ~`Idade` + `Sexo`+
                 `Há quanto tempo trabalha no 'Local de trabalho 1'?` +
@@ -105,7 +143,9 @@ result=compareGroups(`GRUPO MÁSCARA` ~`Idade` + `Sexo`+
                 `Quantas doses da vacina contra a COVID-19 já tomou? (A1)`+
                 `Tomou a vacina contra a gripe (este ano)? (A1)`+
                 `STATUS VACINAL - A2: Já tomou vacina contra a COVID-19?`+
-                `Quantas doses da vacina contra a COVID-19 já tomou? (A2)`,data=df)
+                `Quantas doses da vacina contra a COVID-19 já tomou? (A2)`,
+              data=df,
+              method = 4)
 
 
 # Para as variáveis numericas Idade,peso,altura e IMC o pacote utiliza 
